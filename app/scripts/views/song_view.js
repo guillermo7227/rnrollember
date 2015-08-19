63d51b37b8b2e3a4c30d7c793b79025039404204
Guillermo Agudelo <guille.agudelo@gmail.com>
2015-08-18 22:01:09 -0500

    antes de emberdata

---
+++ app/scripts/views/song_view.js
@@ -0,0 +1,21 @@
+RnrollYeoman.SongView = Ember.View.extend({
+
+    didInsertElement: function () {
+
+        $('#song-duration').text(function() {
+
+            var $this = $(this);
+
+            var origSeconds = $this.attr('data-duration');
+
+            var minutes = Math.floor(origSeconds / 60);
+
+            var seconds = Math.floor(origSeconds % 60);
+
+            return minutes + ":" + seconds
+
+        });
+
+    }
+
+});
